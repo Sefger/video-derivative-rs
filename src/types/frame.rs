2a3e79d460fa69f2b22d8ade6e4a0790f8aa7f36
Sefger <sefger2006@gmail.com>
2025-11-20 17:50:01 +0300

    find error

--- src/types/frame.rs
+++ src/types/frame.rs
@@ -1,27 +1,36 @@
-use std::process::Output;
 use image::{ImageBuffer, Rgb, RgbImage};
 
-
 #[derive(Debug, Clone)]
-pub struct VideoFrame{
+pub struct VideoFrame {
     pub data: RgbImage,
-    pub timestamp:f64,
-    pub frame_number:usize
+    pub timestamp: f64,
+    pub frame_number: usize,
 }
-impl VideoFrame{
-    pub fn new (data:RgbImage, frame_number:usize, timestamp:f64)-> Self{
-        Self{
-            data, frame_number, timestamp
+
+impl VideoFrame {
+    pub fn new(data: RgbImage, frame_number: usize, timestamp: f64) -> Self {
+        Self {
+            data,
+            frame_number,
+            timestamp,
         }
     }
-    pub fn width(&self)->u32{
+
+    pub fn width(&self) -> u32 {
         self.data.width()
     }
-    pub fn height(&self)->u32{
+
+    pub fn height(&self) -> u32 {
         self.data.height()
     }
-    pub fn dimension(&self)->(u32, u32){
+
+    pub fn dimensions(&self) -> (u32, u32) {
         (self.data.width(), self.data.height())
+    }
+
+    /// Сохраняет кадр в файл
+    pub fn save_to_file(&self, path: &std::path::Path) -> Result<(), image::ImageError> {
+        self.data.save(path)
     }
 }
 
